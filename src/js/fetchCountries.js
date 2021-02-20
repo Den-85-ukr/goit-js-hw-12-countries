d4bfdb4cc7b57812ffe37539bea87cf0f323c431
Den-85-ukr <madden85@ukr.net>
2021-02-20 16:28:02 +0200

    finaly update

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,6 @@
 function fetchCountries(searchQuery) {
   return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-    .then(response => {
-      if (response.ok) return response.json();
-      else return [];
-    })
+    .then(response => response.ok ? response.json() : [])
     .catch(error => console.log(error));
 }
 
